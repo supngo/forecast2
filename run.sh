88e7a50baba5ead4cfe35351bdb24164b1ad917c
thomPi5 <whizmate81@gmail.com>
2025-10-05 13:57:30 -0400

    updated run.sh and main.css

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 sleep 20
 
-echo -n "Starting Forecast application on Pi 3..."
+echo -e "Starting Forecast application on Pi 3...\n"
 
-/home/pi/.sdkman/candidates/java/current/bin/java -jar /home/pi/Forecast/target/forecast-1.0.jar > /home/pi/Forecast/forecast.log 2>&1 &
+/home/pi/.sdkman/candidates/java/current/bin/java -jar /home/pi/Forecast/target/forecast-1.0.jar >> /home/pi/Forecast/forecast.log 2>&1
 
-echo -n "Done!"
