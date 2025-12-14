98c20cc6e86d59b5cca3e6737074471d9676a72a
pi5_new <whizmate81@gmail.com>
2025-12-13 23:12:05 -0500

    fixed start forecast

--- start_forecast.sh
+++ start_forecast.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
 sleep 90
-chromium --kiosk http://localhost:8080/forecast
+#chromium --kiosk --password-store=basic http://localhost:8080/forecast
+chromium --start-fullscreen --password-store=basic http://localhost:8080/forecast
