d1d10d5d8f14102cc716dd47f8af0c4529a59814
pi5_new <whizmate81@gmail.com>
2025-12-13 22:47:10 -0500

    cleaned code and added script to start forecast

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 20
+sleep 45
 
-echo -e "Starting Forecast application on Pi 3...\n"
+echo -e "Starting Forecast application on Pi 5...\n"
 
 /home/pi/.sdkman/candidates/java/current/bin/java -jar /home/pi/Forecast/target/forecast-2.0.jar >> /home/pi/Forecast/forecast.log 2>&1
 
---
+++ start_forecast.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 90
+chromium --kiosk http://localhost:8080/forecast
