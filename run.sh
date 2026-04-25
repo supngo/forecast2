82b36556a43f5a6aac4abe67e0ed3974ff6f4e97
Thom Ngo <supngo@yahoo.com>
2026-04-25 19:01:16 -0400

    updated version

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ sleep 45
 
 echo -e "Starting Forecast application on Pi 5...\n"
 
-/home/pi/.sdkman/candidates/java/current/bin/java -jar /home/pi/Forecast/target/forecast-2.0.jar >> /home/pi/Forecast/forecast.log 2>&1
+/home/pi/.sdkman/candidates/java/current/bin/java -jar /home/pi/Forecast/target/forecast-3.0.jar >> /home/pi/Forecast/forecast.log 2>&1
 
